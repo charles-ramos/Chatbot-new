a3f24f895ceec5e90eeb36a09b49b367a3f36ec6
Qoshawa <138309197+Qoshawa@users.noreply.github.com>
2023-10-03 23:18:20 +0100

    Update __init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -10,7 +10,6 @@ import nltk
 import pickle
 import json
 from tensorflow.keras.models import load_model
-import csv
 
 from nltk.stem import WordNetLemmatizer
 lemmatizer=WordNetLemmatizer()
@@ -20,7 +19,6 @@ intents = json.loads(open('chatbot_codes/intents.json').read())
 #intents = json.loads(open('chatbot_codes\intents.json', encoding='cp1252', errors='ignore').read())
 words = pickle.load(open('chatbot_codes/words.pkl','rb'))
 classes = pickle.load(open('chatbot_codes/classes.pkl','rb'))
-csv_file = csv.reader(open('chatbot_codes\GeneralProductPricing.csv','r'))
 
 if __name__=='__main__':
     app.run()
