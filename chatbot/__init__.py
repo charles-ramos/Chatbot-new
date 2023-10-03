a4d2a086d1a9d13bcdc7831a019368457b4d9d93
Qoshawa <138309197+Qoshawa@users.noreply.github.com>
2023-10-03 22:51:26 +0100

    Update __init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -5,17 +5,17 @@ from chatbotconfig import Config
 app=Flask(__name__)
 app.config.from_object(Config)
 
-import keras
+import tensorflow.keras
 import nltk
 import pickle
 import json
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import csv
 
 from nltk.stem import WordNetLemmatizer
 lemmatizer=WordNetLemmatizer()
 
-model=load_model('chatbot_codes/mymodel.h5')
+model=load_model('chatbot_codes/mymodel.h5', compile = False)
 intents = json.loads(open('chatbot_codes/intents.json').read())
 #intents = json.loads(open('chatbot_codes\intents.json', encoding='cp1252', errors='ignore').read())
 words = pickle.load(open('chatbot_codes/words.pkl','rb'))
