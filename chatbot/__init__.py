7b19ab0a5c700f7f71e7e851f1b9d82cc624682e
Qoshawa <138309197+Qoshawa@users.noreply.github.com>
2023-10-11 14:45:23 +0100

    Update __init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -5,7 +5,7 @@ from chatbotconfig import Config
 app=Flask(__name__)
 app.config.from_object(Config)
 
-import keras
+import tensorflow
 import nltk
 import pickle
 import json
@@ -20,13 +20,11 @@ lemmatizer=WordNetLemmatizer()
 
 model=load_model('chatbot_codes/mymodel.h5', compile = False)
 intents = json.loads(open('chatbot_codes/intents.json').read())
-#intents = json.loads(open('chatbot_codes\intents.json', encoding='cp1252', errors='ignore').read())
 words = pickle.load(open('chatbot_codes/words.pkl','rb'))
 classes = pickle.load(open('chatbot_codes/classes.pkl','rb'))
-#csv_file = csv.reader(open('chatbot_codes/GeneralProductPricing.csv','r'))
 
 
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=8181)
+#if __name__=='__main__':
+#    app.run(debug=True,host='0.0.0.0',port=8181)
     
 from chatbot import routes
